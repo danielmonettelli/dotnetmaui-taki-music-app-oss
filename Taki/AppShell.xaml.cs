111b072ddb33dc517e660afbfe9eff89e0bb6650
Daniel Monettelli <danielmonetelli@hotmail.com>
2024-02-22 19:36:43 -0500

    🎉 [Code] Added initial .NET MAUI template

---
+++ Taki/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Taki
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Taki/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Taki
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
